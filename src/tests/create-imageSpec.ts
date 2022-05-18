8d3f8aa0e0d3998782dbbc3bec4a32e4a5b41a02
basma ebraheim <basmaebraheim@gmail.com>
2022-05-18 15:37:42 +0200

    update

--- src/tests/create-imageSpec.ts
+++ src/tests/create-imageSpec.ts
@@ -2,6 +2,9 @@ import createImage from '../helpers/create-image';
 
 describe('test createImage ', (): void => {
   describe('validate image', (): void => {
+    it('check if not throw error', async (): Promise<void> => {
+      expect(async () => { await createImage("encenadaport", 100, 100, "encenadaport(100x100)");}).not.toThrow();
+    });
     it('check if valid image name return true', async (): Promise<void> => {
       const createImageResponse = await createImage("encenadaport", 100, 100, "encenadaport(100x100)");
       expect(createImageResponse).toBe(true);
