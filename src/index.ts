9628db160615335d6765374798280e83501ea176
basma ebraheim <basmaebraheim@gmail.com>
2022-05-18 02:57:23 +0200

    add node & nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("test")
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.get("/api", (req, res) => {
+    res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+    console.log("server started at localhost:${port}");
+});
